4a60cd455fd3c6dc6fb5b4afa8629adf6eea5e25
Vlado <iliya5376@mail.com>
2022-06-13 23:20:49 +0300

    create template

---
+++ src/card/index.js
@@ -0,0 +1,18 @@
+export const Card = () => (
+    <div className="Card">
+        <div className="MainInfo">
+            <img
+                className="Icon"
+                src="https://openweathermap.org/img/wn/10d@2x.png"
+                alt="icon"
+            />
+            <div className="Title">New York</div>
+            <div className="Description">Cloudy</div>
+            <div className="Temperature">20</div>
+        </div>
+        <div className="Information">
+            <div>Humidity: 15</div>
+            <div>Feels like: 19</div>
+        </div>
+    </div>
+);
---
+++ src/input/index.js
@@ -0,0 +1,6 @@
+export const Input = () => (
+    <div className="InputWrap">
+        <input className="Input"/>
+        <button className="Button">+</button>
+    </div>
+);
